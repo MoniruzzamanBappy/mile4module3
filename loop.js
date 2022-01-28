5ae4817a2c4269ac095e8538065d1d5b1bf5aefd
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-01-28 22:46:56 +0600

    first

---
+++ loop.js
@@ -0,0 +1,17 @@
+var alurDam = 0;
+
+while (alurDam < 10) {
+    console.log('first=', alurDam);
+    alurDam++;
+    console.log('second=', alurDam);
+}
+
+var piajErDam;
+for (piajErDam = 0; piajErDam < 10; piajErDam++) {
+    console.log('Piaj er Dam=', piajErDam);
+}
+
+var i = 5;
+for (i; i < 10; i++) {
+    console.log('value of i=', i);
+}
